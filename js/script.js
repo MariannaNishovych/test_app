9170d0f2d10ce774d31726af852a464ab2530b8f
Marianna Nishovych <nisovicmarianna@gmail.com>
2025-02-10 21:12:44 +0200

    add first step

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 'use strict';
 
-let number = 5;
+// let number = 5;
 
-const leftBorderWidth = 1;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-console.log(typeof 123n);
+// console.log(typeof 123n);
+
+const numberOfFilms = +prompt('How many films do you watch?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt('One of the last films you watched?', ''),
+      b = prompt('How much would you rate it?', ''),
+      c = prompt('One of the last films you watched?', ''),
+      d = prompt('How much would you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
